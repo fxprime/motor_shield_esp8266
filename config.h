88905101d01c16543b5705dbd0a9ce223c908f0b
fxprime <paheyisoicus@gmail.com>
2021-03-25 21:39:58 +0700

    Change wifi name and token for instructive

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 
-const char* ssid = "Kanuengnit_kiss2G";
-const char* pwd = "3213213213";
-const char* token = "2GtF5ge_moc3FOACLvL3d_5E8SdcLGJD";
+const char* ssid = "NAME_WIFI";
+const char* pwd = "PASSWARD_WIFI";
+const char* token = "BLYNK_TOKEN";
 
-int control[2] = {0,0};
+int control[2] = {512,512};
